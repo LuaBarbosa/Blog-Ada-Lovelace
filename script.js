a3c6778f9036d48ddd294734b520edf2e02ff7f9
LuaBarbosa <lua.barbosa@yahoo.com.br>
2021-06-01 20:37:08 -0300

    Feat: Criando validação do formulário.

---
+++ script.js
@@ -0,0 +1,15 @@
+document
+  .getElementById("botaoEnviar")
+  .addEventListener("click", validaFormulario);
+
+function validaFormulario() {
+  if (
+    document.getElementById("usuario").value != "" &&
+    document.getElementById("email").value != "" &&
+    document.getElementById("telefone").value != ""
+  ) {
+    alert("Prontinho! Você receberá as novidades por email");
+  } else {
+    alert("Por favor, preencha os campos nome, email e telefone!");
+  }
+}
